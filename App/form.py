41e5ad8c37329b26f8a697cdceba91cfd9d2c42b
Jack Harper <jack.harper@stfc.ac.uk>
2018-07-25 09:59:27 +0100

    adding default value for message check

--- App/form.py
+++ App/form.py
@@ -6,7 +6,6 @@ from wtforms.validators import DataRequired
 class LoginForm(FlaskForm):
     Field1 = StringField('Broker', validators=[DataRequired()])
     Field2 = StringField('Topic', validators=[DataRequired()])
-    CheckBox = BooleanField('Check low and high offsets?')
-    IntField = IntegerField('Messages to retrieve (default - all)', default=0)
-    submit = SubmitField('Submit')
+    IntField = IntegerField('Messages to retrieve (default - 0 - all)', default=0)
+    check_messages = SubmitField('Show messages')
     check_high_low = SubmitField('Check the high and low offsets')
