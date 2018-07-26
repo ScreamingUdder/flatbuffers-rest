4f08838befdabf2918abb1c3bbcea4da7dd277d2
James Page <jamesapage@btinternet.com>
2018-07-26 15:00:12 +0100

    \ to os.path.join

--- App/deserializer.py
+++ App/deserializer.py
@@ -1,16 +1,17 @@
 from App.schema_mappings import schema_map
 from subprocess import run
-
+from os import path
 
 def deserialize_flatbuffers(message_value):
     file_identifier = (message_value[4:8])
     file_name = schema_map[file_identifier]
-    with open('./temp', 'wb')as file:
+    with open(path.join('.', 'temp'), 'wb')as file:
         file.write(message_value)
-    flatc_args = ('flatc --json .\schemas\\'+file_name + ' -- temp')
+    location = path.join('.', 'schemas', file_name)
+    flatc_args = ('flatc --json '+location +' -- temp')
     print(flatc_args)
     run(flatc_args, shell=True)
-    with open('./temp.json', 'r') as file:
+    with open(path.join('.', 'temp.json',), 'r') as file:
         read_message = file.read()
-    read_message = read_message.replace('\n','')
+    read_message = read_message.replace('\n', '')
     return read_message
