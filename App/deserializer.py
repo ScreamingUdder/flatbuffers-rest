b585309ee73dfd05cf4bd2618bcb2e178c486f68
James Page <jamesapage@btinternet.com>
2018-07-26 15:31:38 +0100

    added streaming data types as submodule

--- App/deserializer.py
+++ App/deserializer.py
@@ -7,7 +7,7 @@ def deserialize_flatbuffers(message_value):
     file_name = schema_map[file_identifier]
     with open(path.join('.', 'temp'), 'wb')as file:
         file.write(message_value)
-    location = path.join('.', 'schemas', file_name)
+    location = path.join('.', 'streaming-data-types', 'schemas', file_name)
     flatc_args = ('flatc --json '+location +' -- temp')
     print(flatc_args)
     run(flatc_args, shell=True)
