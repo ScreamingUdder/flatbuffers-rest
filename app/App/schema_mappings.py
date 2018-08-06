8bcaec70b52c6ccb75bb3da6afb783ef2b866a45
Matthew D Jones <matthew.d.jones@tessella.com>
2018-08-06 13:01:49 +0100

    Update path to schema

--- app/App/schema_mappings.py
+++ app/App/schema_mappings.py
@@ -3,7 +3,7 @@ from os import path
 
 
 def create_schema_map():
-    schemas = os.listdir(path.join('..', 'streaming-data-types', 'schemas'))
+    schemas = os.listdir(path.join('..', '..', 'streaming-data-types', 'schemas'))
     dynamic_schemas = {}
     for files in schemas:
         dynamic_schemas[bytes(files[0:4], 'utf-8')] = files
